c65ebf40283586b62b800937e8fa6bfe6a30f539
Jamie Geddes <jamie@clumsypenguin.com>
2017-05-31 23:11:42 +0100

    Initial test implementation of a failed call to a url

---
+++ WebServiceCalls/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebServiceCalls
+{
+    public class Program
+    {
+        public static void Main(string[] args) => Execute().GetAwaiter().GetResult();
+
+        private static async Task Execute()
+        {
+            var checker = new WebsiteChecker();
+
+            await checker.CallWebsite();
+
+            Console.WriteLine("done");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebServiceCalls/WebsiteChecker.cs
@@ -0,0 +1,38 @@
+﻿namespace WebServiceCalls
+{
+    using Polly;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class WebsiteChecker
+    {
+        public async Task CallWebsite()
+        {
+            var policy = Policy.Handle<HttpRequestException>().RetryAsync(3);
+
+            try
+            {
+                await policy.ExecuteAsync(CheckWebsite);
+            }
+            catch(HttpRequestException)
+            {
+                Console.WriteLine("Exceeded maximum number of retries, operation failed");
+            }
+        }
+
+        private async Task CheckWebsite()
+        { 
+            Console.WriteLine("calling unreachable site...");
+
+            using (var client = new HttpClient())
+            {
+                const string uri = "https://unreachable.example.com";
+
+                var response = await client.GetAsync(uri);
+
+                Console.WriteLine($"response: {response.StatusCode}");
+            }
+        }
+    }
+}
